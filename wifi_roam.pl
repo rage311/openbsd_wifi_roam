7509b271f8bf1e77dbfee5185185d123525ca1f5
rage311 <rage_311@hotmail.com>
2017-06-16 11:55:48 -0600

    Die if wifi details can't be parsed Exit code 0 if there's not a stronger BSSID to roam to

--- wifi_roam.pl
+++ wifi_roam.pl
@@ -12,7 +12,7 @@ say 'Reminder: Need to run as root';
 #TODO: any way to pledge to reduce permissions?
 
 die 'Need only alphanumeric interface id, eg "iwm0"'
-  unless ((my $interface = shift) =~ /^[a-z]+\d+$/);
+  unless (my $interface = shift) =~ /^[a-z]+\d+$/;
 
 # read output from ifconfig
 open my $ifconfig_fh, '-|', "/sbin/ifconfig $interface scan" or die "$!";
@@ -20,21 +20,23 @@ my @ifconfig_input = <$ifconfig_fh>;
 close $ifconfig_fh;
 
 my %current_wifi;
-while (my $line = shift @ifconfig_input) {
+while ((my $line = shift @ifconfig_input) && !%current_wifi) {
   # look for current wifi line
   next unless $line =~ /^\s*ieee80211: (?<wifi_details>.+)$/;
 
-  $+{wifi_details} =~ m{
-    ^ nwid  \s  (?<nwid>.+)             \s
-      chan  \s  (?<chan>\d+)            \s
-      bssid \s  (?<bssid>[\da-f:]+)     \s
-                ((?<signal_pct>\d+)%)?  $
-  }x;
+  die 'Unable to parse current wifi details'
+    unless $+{wifi_details} =~ m{
+      ^ nwid  \s  (?<nwid>.+)             \s
+        chan  \s  (?<chan>\d+)            \s
+        bssid \s  (?<bssid>[\da-f:]+)     \s?
+                  ((?<signal_pct>\d+)%)?  $
+    }x;
 
   say 'Current wifi:';
   p %+;
   print "\n";
-  last if %current_wifi = %+;
+
+  %current_wifi = %+;
 }
 
 die 'Not able to parse wifi details' unless %current_wifi;
@@ -50,21 +52,23 @@ my $threshold = ($current_wifi{signal_pct} // 0) + 1;
 
 my %new_wifi;
 # check the rest of ifconfig's scan output for a bssid with a higher signal str
-for my $line (@ifconfig_input) {
+#for my $line (@ifconfig_input) {
+while ((my $line = shift @ifconfig_input) && !%new_wifi) {
   next unless $line =~ m{
     chan  \s  (?<chan>\d+)         \s
     bssid \s  (?<bssid>[\da-f:]+)  \s
               (?<signal_pct>\d+)%
   }x;
 
-  # die at first match not greater than threshold, since it's ordered
+  # exit at first match not greater than threshold, since it's ordered
   # highest to lowest signal strength
-  die 'No stronger bssid to roam to' unless $+{signal_pct} > $threshold;
+  say 'No stronger bssid to roam to' and exit 0
+    unless $+{signal_pct} > $threshold;
 
   say 'Roaming to:';
   p %+;
 
-  last if %new_wifi = %+;
+  %new_wifi = %+;
 }
 
 #TODO: still need to check permissions for setting new chan and bssid
@@ -73,6 +77,6 @@ die 'Failed to roam'
   unless system(
     '/sbin/ifconfig',
     "$interface",
-    'chan', "$new_wifi{chan}",
-    'bssid', "$new_wifi{bssid}"
+    'chan',   "$new_wifi{chan}",
+    'bssid',  "$new_wifi{bssid}"
   ) == 0;
